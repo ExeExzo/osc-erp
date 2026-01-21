7274f8e0a71ec0d28281014f696b1b65f3dd9011
ExeExzo <alimjan.prom@gmail.com>
2026-01-21 10:39:39 +0500

    Connect render database

--- osc_erp/settings.py
+++ osc_erp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,9 @@ SECRET_KEY = 'django-insecure-xx_&1u502uwv%9#b)8n93gt=n%*f+y56)^qvzvi4)!+zvk&$tb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://osc-erp.onrender.com"
+]
 
 
 AUTH_USER_MODEL = 'user.User'
@@ -52,6 +56,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -93,15 +98,23 @@ WSGI_APPLICATION = 'osc_erp.wsgi.application'
 # }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'osc_erp',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin12345',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'osc_erp',
-        'USER': 'postgres',
-        'PASSWORD': 'admin12345',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
